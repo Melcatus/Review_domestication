850779f453e2db99f71595529620d7d3eadc3ca6
Melania Vega <36024079+Melcatus@users.noreply.github.com>
2020-09-13 01:15:42 -0500

    Update 1_fastqc_sample.sh

--- bin/1_fastqc_sample.sh
+++ bin/1_fastqc_sample.sh
@@ -4,8 +4,14 @@
 
 # Create output directory
 mkdir ../data/soja/fastqcs 
+mkdir ../data/rice/fastqcs 
 
-## fastqc
+## fastqc to sample of soja 
 for i in ../data/soja/*.fastq.gz;
 do fastqc $i -o ../data/soja/fastqcs
+done
+
+## fastqc to sample of rice 
+for i in ../data/rice/*.fastq.gz;
+do fastqc $i -o ../data/rice/fastqcs
 done
